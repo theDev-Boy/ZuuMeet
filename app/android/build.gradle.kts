5533fd0c713347784e6f8c7b2aa4bc851ef120a4
MuhammadAfnan02 <mustafa.afnan01@gmail.com>
2026-05-10 14:47:38 +0500

    Fixed Gradel

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -21,11 +21,11 @@ subprojects {
 
 // Fix for plugins that still use deprecated package= in AndroidManifest.xml (AGP 8.11+)
 subprojects {
-    afterEvaluate {
-        if (project.plugins.hasPlugin("com.android.library")) {
-            val libExt = project.extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)
+    val fixNamespace = Action<Project> { proj ->
+        if (proj.plugins.hasPlugin("com.android.library")) {
+            val libExt = proj.extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)
             if (libExt != null && (libExt.namespace == null || libExt.namespace!!.isEmpty())) {
-                val manifestFile = project.file("src/main/AndroidManifest.xml")
+                val manifestFile = proj.file("src/main/AndroidManifest.xml")
                 if (manifestFile.exists()) {
                     val content = manifestFile.readText()
                     val match = Regex("""package\s*=\s*"([^"]+)"""").find(content)
@@ -35,6 +35,12 @@ subprojects {
                 }
             }
         }
+    }
+    
+    if (project.state.executed) {
+        fixNamespace.execute(project)
+    } else {
+        project.afterEvaluate(fixNamespace)
     }
 }
 
